2b7c1001fe3f53fcc419f95918d82c429fe599b7
fkrzywka <filip.krzywka@nokia.com>
2018-06-06 10:34:52 +0200

    Invalid class name corrected
    Licenses added to client simulator files
    
    Change-Id: I07afa358daf25cb36c1ed1523f17b5bb2fad40b5
    Signed-off-by: fkrzywka <filip.krzywka@nokia.com>
    Issue-ID: DCAEGEN2-601

--- hv-collector-client-simulator/src/test/kotlin/org.onap.dcae.collectors.veshv.main/MessageFactoryTest.kt
+++ hv-collector-client-simulator/src/test/kotlin/org.onap.dcae.collectors.veshv.main/MessageFactoryTest.kt
@@ -1,6 +1,26 @@
+/*
+ * ============LICENSE_START=======================================================
+ * dcaegen2-collectors-veshv
+ * ================================================================================
+ * Copyright (C) 2018 NOKIA Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
 package org.onap.dcae.collectors.veshv.main
 
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.onap.dcae.collectors.veshv.main.impl.MessageFactory
@@ -10,22 +30,22 @@ import kotlin.test.assertEquals
  * @author Jakub Dudycz <jakub.dudycz@nokia.com>
  * @since June 2018
  */
-object WireFrameTest : Spek({
+object MessageFactoryTest : Spek({
+    describe("message factory") {
+        val factory = MessageFactory()
 
-    val factory = MessageFactory()
+        given("no parameters") {
+            it("should return flux with one message") {
+                val result = factory.createMessageFlux()
 
-
-    given("no parameters") {
-        it("should return flux with one message") {
-            val result = factory.createMessageFlux()
-
-            assertEquals(1, result.count().block())
+                assertEquals(1, result.count().block())
+            }
         }
-    }
-    given("messages amount") {
-        it("should return message flux of specified size") {
-            val result = factory.createMessageFlux(5)
-            assertEquals(5, result.count().block())
+        given("messages amount") {
+            it("should return message flux of specified size") {
+                val result = factory.createMessageFlux(5)
+                assertEquals(5, result.count().block())
+            }
         }
     }
 })
