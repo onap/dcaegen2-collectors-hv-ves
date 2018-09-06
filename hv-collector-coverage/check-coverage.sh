bbd996fad71efee3fb45a0456d5f5ce11a36fd20
Piotr Jaszczyk <piotr.jaszczyk@nokia.com>
2018-09-06 08:20:03 +0200

    Build enhancements
    * Handling docker proxy configuration in a reasonable way
    * Should skip coverage verification when skipTests was set
    * Get rid of resources encoding warning
    * Fix check-coverage output on Jenkins
    
    Change-Id: If828fc41a88074931649e739c615a8b9d8db8739
    Issue-ID: DCAEGEN2-755
    Signed-off-by: Piotr Jaszczyk <piotr.jaszczyk@nokia.com>

--- hv-collector-coverage/check-coverage.sh
+++ hv-collector-coverage/check-coverage.sh
@@ -3,10 +3,11 @@ set -euo pipefail
 
 JACOCO_REPORT="$1"
 MIN_COVERAGE_PERCENT="$2"
+LOG_FILE=target/check-coverage.log
 
 function coverage_from_report() {
   local xpath_expr="string(/report/counter[@type='INSTRUCTION']/@$1)"
-  xpath -q -e "$xpath_expr" "$JACOCO_REPORT"
+  xpath -q -e "$xpath_expr" "$JACOCO_REPORT" 2>> ${LOG_FILE}
 }
 
 missed=`coverage_from_report missed`
@@ -14,9 +15,13 @@ covered=`coverage_from_report covered`
 total=$(($missed + $covered))
 coverage=$((100 * $covered / $total))
 
+echo "Warnings from xpath evaluation:"
+cat ${LOG_FILE}
+echo
+
 echo "Coverage: $coverage% (covered/total: $covered/$total)"
 
-if [[ $coverage -lt $MIN_COVERAGE_PERCENT ]]; then
+if [[ ${coverage} -lt ${MIN_COVERAGE_PERCENT} ]]; then
   echo "Coverage is too low. Minimum coverage: $MIN_COVERAGE_PERCENT%"
   exit 1
 fi
