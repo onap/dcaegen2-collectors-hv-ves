ccdbec825673153bd57681fa8e1e5a507f4f776b
Filip Krzywka <filip.krzywka@nokia.com>
2018-12-12 08:14:57 +0100

    Add helper scripts
    Change-Id: I8d5b48511e1b7f9cb7d1f65e8605ebe4cf1b17fc
    Issue-ID: DCAEGEN2-1027
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

---
+++ development/bin/dcae-reset.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+# ============LICENSE_START=======================================================
+# dcaegen2-collectors-veshv
+# ================================================================================
+# Copyright (C) 2018 NOKIA
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+set -euo pipefail
+
+
+usage() {
+    echo "Resets dcae-app-simulator consumed messages count"
+    echo "Usage: $0 [-h|--help] [-v|--verbose]"
+    exit 1
+}
+
+optspec=":vh-:" # catch v, h and -
+while getopts "$optspec" arg; do
+    case "${arg}" in
+        -) # handle longopts
+            case "${OPTARG}" in
+                verbose)
+                    VERBOSE=True
+                    ;;
+                help)
+                    usage
+                    ;;
+                *)
+                    echo "Unknown option --${OPTARG}" >&2
+                    usage
+                    ;;
+             esac
+             ;;
+        v)
+            VERBOSE=True
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            echo "Unknown option -${OPTARG}" >&2
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -n "${VERBOSE+x}" ]; then
+    echo "Requesting DCAE app running on port 6063 to reset messages count"
+fi
+
+curl --request DELETE localhost:6063/messages
+echo
---
+++ development/bin/run-xnf-simulator.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+# ============LICENSE_START=======================================================
+# dcaegen2-collectors-veshv
+# ================================================================================
+# Copyright (C) 2018 NOKIA
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+set -euo pipefail
+
+usage() {
+    echo "Start xnf-simulator container on given port and inside of given docker-network (by default 'development_default')"
+    echo "Usage: $0 [-h|--help] [-v|--verbose] <xnf listen port> [<hv ves docker network>]"
+    exit 1
+}
+
+optspec=":vh-:" # catch v, h and -
+while getopts "$optspec" arg; do
+    case "${arg}" in
+        -) # handle longopts
+            case "${OPTARG}" in
+                verbose)
+                    VERBOSE=True
+                    ;;
+                help)
+                    usage
+                    ;;
+                *)
+                    echo "Unknown option --${OPTARG}" >&2
+                    usage
+                    ;;
+             esac
+             ;;
+        v)
+            VERBOSE=True
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            echo "Unknown option -${OPTARG}" >&2
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+[ $# -eq 0 ] && usage
+
+
+LISTEN_PORT=$1
+HV_VES_NETWORK=${2:-development_default}
+
+PORTS="${LISTEN_PORT}:${LISTEN_PORT}/tcp"
+HV_VES_REPO_HOME=`pwd`/..
+
+if [ -n "${VERBOSE+x}" ]; then
+    echo "Starting xnf-simulator with ports configuration: ${PORTS} on network: ${HV_VES_NETWORK}"
+    echo "Container id:"
+fi
+docker run -d \
+           -v ${HV_VES_REPO_HOME}/ssl/:/etc/ves-hv/ \
+           -p ${PORTS} \
+           --network ${HV_VES_NETWORK} \
+           onap/org.onap.dcaegen2.collectors.hv-ves.hv-collector-xnf-simulator \
+                    --listen-port ${LISTEN_PORT} \
+                    --ves-host ves-hv-collector \
+                    --ves-port 6061 \
+                    --key-store-password onaponap \
+                    --trust-store-password onaponap
