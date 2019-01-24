622517ce7268f52fd3c5f6b5b0afb3a30a9e2f25
Filip Krzywka <filip.krzywka@nokia.com>
2019-01-24 11:21:26 +0100

    Correct totalConnections metric
    In previous implementation performed healthcheck on container
    was counted as client connection, because metric was notified
    for every TCP channel opened. This was making this metric less useful
    (to avoid saying "useless" ;) ).
    
    - refactored NettyTcpServer trying to extract functions with logic
    not so strictly related to HV-VES behavior
    - some renames and methods splitting was made in attempt to make code
    more readable
    - hv-ves should not count connections from either "127.0.0.1" or
    "localhost" to his `totalConnections` metric
    - removed redundant logging by adding new methods to Logger
    
    Change-Id: I5f10dac8dac82eafd3b0de92a7ec43f2c23b8c16
    Issue-ID: DCAEGEN2-1119
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

---
+++ sources/hv-collector-core/src/main/kotlin/org/onap/dcae/collectors/veshv/impl/socket/networking.kt
@@ -0,0 +1,60 @@
+/*
+ * ============LICENSE_START=======================================================
+ * dcaegen2-collectors-veshv
+ * ================================================================================
+ * Copyright (C) 2019 NOKIA
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcae.collectors.veshv.impl.socket
+
+import arrow.core.Option
+import arrow.syntax.collections.firstOption
+import io.netty.handler.ssl.SslHandler
+import io.netty.util.concurrent.Future
+import reactor.netty.ByteBufFlux
+import reactor.netty.Connection
+import reactor.netty.NettyInbound
+import java.net.InetAddress
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLSession
+
+internal fun InetAddress.isLocalClientAddress() = hostAddress == "127.0.0.1" || hostName == "localhost"
+
+internal fun Connection.getSslSession(): Option<SSLSession> =
+        Option.fromNullable(
+                channel()
+                        .pipeline()
+                        .get(SslHandler::class.java)
+                        ?.engine()
+                        ?.session
+        )
+
+internal fun SSLSession.findClientCert(): Option<X509Certificate> =
+        peerCertificates
+                .firstOption()
+                .flatMap { Option.fromNullable(it as? X509Certificate) }
+
+internal fun withConnectionFrom(nettyInboud: NettyInbound, task: (Connection) -> Unit) =
+        nettyInboud.withConnection(task)
+
+internal fun Connection.closeChannel() = channel().close()
+
+internal fun Connection.closeChannelAnd(task: (Future<in Void>) -> Unit) =
+        closeChannel().addListener { task(it) }
+
+internal fun <T> NettyInbound.closeConnectionAndReturn(returnValue: T): T =
+        withConnectionFrom(this) { it.closeChannel() }.let { returnValue }
+
+internal fun NettyInbound.createDataStream(): ByteBufFlux = receive().retain()
