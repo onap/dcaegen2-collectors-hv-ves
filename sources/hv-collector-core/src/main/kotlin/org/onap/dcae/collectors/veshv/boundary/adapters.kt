821fecd7fcfcd6165510b0160737d0526243b889
kjaniak <kornel.janiak@nokia.com>
2019-03-21 14:03:53 +0100

    Use DataStream API from CBS client
    Change-Id: Ief92f793282288938c6663616e9613c6df2d8ddb
    Issue-ID: DCAEGEN2-1346
    Signed-off-by: kjaniak <kornel.janiak@nokia.com>

--- sources/hv-collector-core/src/main/kotlin/org/onap/dcae/collectors/veshv/boundary/adapters.kt
+++ sources/hv-collector-core/src/main/kotlin/org/onap/dcae/collectors/veshv/boundary/adapters.kt
@@ -27,6 +27,7 @@ import org.onap.dcae.collectors.veshv.model.ClientRejectionCause
 import org.onap.dcae.collectors.veshv.model.ConsumedMessage
 import org.onap.dcae.collectors.veshv.model.MessageDropCause
 import org.onap.dcae.collectors.veshv.utils.Closeable
+import org.onap.dcaegen2.services.sdk.rest.services.cbs.client.model.streams.dmaap.KafkaSink
 import reactor.core.publisher.Flux
 
 interface Sink {
