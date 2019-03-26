4b3fa85a7780d672b27f82707b449d91960c50a1
Filip Krzywka <filip.krzywka@nokia.com>
2019-03-26 13:57:11 +0100

    Move routing functionality inside Router
    - also removed Routing-DSL as it won't be needed anymore
    
    Change-Id: Ifc7bc7641a60936b5257c0bff7a8c51dddc30687
    Issue-ID: DCAEGEN2-1347
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

--- sources/hv-collector-ct/src/test/kotlin/org/onap/dcae/collectors/veshv/tests/fakes/configuration.kt
+++ sources/hv-collector-ct/src/test/kotlin/org/onap/dcae/collectors/veshv/tests/fakes/configuration.kt
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * dcaegen2-collectors-veshv
  * ================================================================================
- * Copyright (C) 2018 NOKIA
+ * Copyright (C) 2018-2019 NOKIA
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
