d529158afb956d50872594ebbae63421e2024486
Filip Krzywka <filip.krzywka@nokia.com>
2019-04-05 08:44:52 +0200

    Allow retrieving multiple kafka topics status
    Change-Id: I5e8433873e5d594e6df9da8c4893b0f54614efae
    Issue-ID: DCAEGEN2-1399
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

--- sources/hv-collector-dcae-app-simulator/src/main/kotlin/org/onap/dcae/collectors/veshv/simulators/dcaeapp/impl/adapters/DcaeAppApiServer.kt
+++ sources/hv-collector-dcae-app-simulator/src/main/kotlin/org/onap/dcae/collectors/veshv/simulators/dcaeapp/impl/adapters/DcaeAppApiServer.kt
@@ -19,6 +19,7 @@
  */
 package org.onap.dcae.collectors.veshv.simulators.dcaeapp.impl.adapters
 
+import arrow.core.Option
 import org.onap.dcae.collectors.veshv.simulators.dcaeapp.impl.DcaeAppSimulator
 import org.onap.dcae.collectors.veshv.utils.NettyServerHandle
 import org.onap.dcae.collectors.veshv.utils.ServerHandle
@@ -26,11 +27,13 @@ import org.onap.dcae.collectors.veshv.utils.http.HttpConstants
 import org.onap.dcae.collectors.veshv.utils.http.HttpStatus
 import org.onap.dcae.collectors.veshv.utils.http.Response
 import org.onap.dcae.collectors.veshv.utils.http.Responses
+import org.onap.dcae.collectors.veshv.utils.http.Responses.stringResponse
 import org.onap.dcae.collectors.veshv.utils.http.sendAndHandleErrors
 import org.onap.dcae.collectors.veshv.utils.http.sendOrError
 import org.onap.dcae.collectors.veshv.utils.logging.Logger
 import reactor.core.publisher.Mono
 import reactor.netty.http.server.HttpServer
+import reactor.netty.http.server.HttpServerRequest
 import reactor.netty.http.server.HttpServerRoutes
 import java.net.InetSocketAddress
 
@@ -39,20 +42,6 @@ import java.net.InetSocketAddress
  * @since May 2018
  */
 class DcaeAppApiServer(private val simulator: DcaeAppSimulator) {
-    private val responseValid by lazy {
-        Responses.statusResponse(
-                name = "valid",
-                message = VALID_RESPONSE_MESSAGE
-        )
-    }
-
-    private val responseInvalid by lazy {
-        Responses.statusResponse(
-                name = "invalid",
-                message = INVALID_RESPONSE_MESSAGE,
-                httpStatus = HttpStatus.BAD_REQUEST
-        )
-    }
 
     fun start(socketAddress: InetSocketAddress, kafkaTopics: Set<String>): Mono<ServerHandle> =
             Mono.defer {
@@ -74,37 +63,49 @@ class DcaeAppApiServer(private val simulator: DcaeAppSimulator) {
                                 res.sendOrError { simulator.listenToTopics(it) }
                             }
                 }
-                .delete("/messages") { _, res ->
-                    logger.info { "Resetting simulator state" }
+                .delete("/messages/{$TOPIC_PARAM_KEY}") { req, res ->
+                    doWithTopicOrReturnInternalErrorResponse(req) {
+                        logger.info { "Resetting simulator state for topic $it" }
+                        simulator.resetState(it)
+                        Mono.just(Responses.Success)
+                    }.let(res::sendAndHandleErrors)
 
-                    res
-                            .header("Content-type", CONTENT_TEXT)
-                            .sendOrError { simulator.resetState() }
                 }
-                .get("/messages/all/count") { _, res ->
-                    logger.info { "Processing request for count of received messages" }
-                    simulator.state().fold(
-                            {
-                                logger.warn { "Error - number of messages could not be specified" }
-                                res.status(HttpConstants.STATUS_NOT_FOUND)
-                            },
-                            {
-                                logger.info { "Returned number of received messages: ${it.messagesCount}" }
-                                res.sendString(Mono.just(it.messagesCount.toString()))
-                            }
-                    )
+                .get("/messages/{$TOPIC_PARAM_KEY}/count") { req, res ->
+                    doWithTopicOrReturnInternalErrorResponse(req) {
+                        logger.info { "Processing request for count of received messages for topic $it" }
+                        simulator.state(it)
+                                .fold({
+                                    val errorMessage = { COUNT_NOT_RESOLVED_MESSAGE + ". Reason: ${it.message}" }
+                                    logger.warn(errorMessage)
+                                    Mono.just(Responses.statusResponse(
+                                            name = "Count not found",
+                                            message = errorMessage(),
+                                            httpStatus = HttpStatus.NOT_FOUND
+                                    )
+                                    )
+                                }, {
+                                    logger.info { "Returned number of received messages: ${it.messagesCount}" }
+                                    Mono.just(
+                                            Responses.stringResponse(
+                                                    message = it.messagesCount.toString(),
+                                                    httpStatus = HttpStatus.OK
+                                            )
+                                    )
+                                })
+                    }.let(res::sendAndHandleErrors)
                 }
-                .post("/messages/all/validate") { req, res ->
+                .post("/messages/{$TOPIC_PARAM_KEY}/validate") { req, res ->
                     req
                             .receive().aggregate().asInputStream()
-                            .map {
-                                logger.info { "Processing request for message validation" }
-                                simulator.validate(it)
-                                        .map(::resolveValidationResponse)
+                            .map { inputStream ->
+                                doWithTopicOrReturnInternalErrorResponse(req) {
+                                    logger.info { "Processing request for message validation" }
+                                    simulator.validate(inputStream, it)
+                                            .map(::resolveValidationResponse)
+                                }
                             }
-                            .flatMap {
-                                res.sendAndHandleErrors(it)
-                            }
+                            .flatMap(res::sendAndHandleErrors)
                 }
                 .get("/healthcheck") { _, res ->
                     val status = HttpConstants.STATUS_OK
@@ -112,6 +113,15 @@ class DcaeAppApiServer(private val simulator: DcaeAppSimulator) {
                     res.status(status).send()
                 }
     }
+
+    private fun doWithTopicOrReturnInternalErrorResponse(req: HttpServerRequest,
+                                                         topicConsumer: (String) -> Mono<Response>) =
+            Option.fromNullable(req.param(TOPIC_PARAM_KEY))
+                    .fold({
+                        Mono.just(
+                                stringResponse("Failed to retrieve parameter from url",
+                                        HttpStatus.INTERNAL_SERVER_ERROR))
+                    }, topicConsumer)
 
     private fun resolveValidationResponse(isValid: Boolean): Response =
             if (isValid) {
@@ -124,10 +134,26 @@ class DcaeAppApiServer(private val simulator: DcaeAppSimulator) {
 
 
     companion object {
-        private const val CONTENT_TEXT = "text/plain"
+        private val logger = Logger(DcaeAppApiServer::class)
         private const val VALID_RESPONSE_MESSAGE = "validation passed"
         private const val INVALID_RESPONSE_MESSAGE = "consumed messages don't match data from validation request"
-        private val logger = Logger(DcaeAppApiServer::class)
+        private const val COUNT_NOT_RESOLVED_MESSAGE = "Error - number of messages could not be specified"
+        private const val TOPIC_PARAM_KEY = "topic"
+
+        private val responseValid by lazy {
+            Responses.statusResponse(
+                    name = "valid",
+                    message = DcaeAppApiServer.VALID_RESPONSE_MESSAGE
+            )
+        }
+
+        private val responseInvalid by lazy {
+            Responses.statusResponse(
+                    name = "invalid",
+                    message = DcaeAppApiServer.INVALID_RESPONSE_MESSAGE,
+                    httpStatus = HttpStatus.BAD_REQUEST
+            )
+        }
     }
 }
 
