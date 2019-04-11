56c52bf4cda0da9bf1768ab894976a3ae902c04c
Izabela Zawadzka <izabela.zawadzka@nokia.com>
2019-04-11 14:27:21 +0200

    Create custom rule to report public modifiers in impl
    Change-Id: I383ca27a835943bd2dc2508425264ad7f64c7725
    Signed-off-by: Izabela Zawadzka <izabela.zawadzka@nokia.com>
    Issue-ID: DCAEGEN2-1430

---
+++ build/hv-collector-analysis/src/main/kotlin/org/onap/dcae/collectors/veshv/analysis/PublicModifiersInImpl.kt
@@ -0,0 +1,71 @@
+/*
+ * ============LICENSE_START=======================================================
+ * dcaegen2-collectors-veshv
+ * ================================================================================
+ * Copyright (C) 2019 NOKIA
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.dcae.collectors.veshv.analysis
+
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.isPublic
+
+class PublicModifiersInImpl(config: Config = Config.empty) : Rule(config) {
+    override val issue: Issue = Issue(javaClass.simpleName, Severity.Maintainability,
+            """
+                Reports public modifiers inside '*.impl' package.
+                """.trimIndent(), Debt(mins=10))
+
+
+    override fun visitKtFile(file: KtFile) {
+        super.visitKtFile(file)
+
+        if(file.packageFqName.toString().contains("impl")) {
+            with(ImplVisitor()){
+                file.accept(this)
+                if(containsPublicDeclarations())
+                    report(CodeSmell(issue, Entity.from(file), REPORT_MESSAGE))
+            }
+        }
+    }
+
+
+    companion object {
+        const val REPORT_MESSAGE = "Implementation files cannot have public declarations"
+    }
+}
+
+private class ImplVisitor: DetektVisitor(){
+    private var publicDeclarations = 0
+
+    fun containsPublicDeclarations() = publicDeclarations > 0
+
+    override fun visitClassOrObject(classOrObject: KtClassOrObject) {
+        if(classOrObject.isPublic){
+            publicDeclarations++
+        }
+    }
+
+    override fun visitNamedFunction(function: KtNamedFunction) {
+        if(function.isTopLevel && function.isPublic){
+            publicDeclarations++
+        }
+    }
+
+    override fun visitProperty(property: KtProperty) {
+        if(property.isPublic) publicDeclarations++
+    }
+}
