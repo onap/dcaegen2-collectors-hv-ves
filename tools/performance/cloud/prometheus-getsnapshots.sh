854377876ba9a39824750902764dba52b3e075ff
mharazin <mateusz.harazin@nokia.com>
2020-04-09 14:32:55 +0200

    Get raw result of stress tests from prometheus
    Issue-ID: DCAEGEN2-1576
    Signed-off-by: Mateusz Harazin <mateusz.harazin@nokia.com>
    Change-Id: I7c18c81f990bd7828e6507d1a7ac6ffc50d0d60e

---
+++ tools/performance/cloud/prometheus-getsnapshots.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# ============LICENSE_START=======================================================
+# dcaegen2-collectors-veshv
+# ================================================================================
+# Copyright (C) 2020 NOKIA
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+ONAP_NAMESPACE=onap
+PROMETHEUS_POD_NAME=$(kubectl -n ${ONAP_NAMESPACE} get pods --no-headers=true -o custom-columns=:metadata.name | grep prometheus-hv-ves-deployment)
+DESTINATION_DIR=./prometheus-snapshots/
+
+IP=$(kubectl config view -o jsonpath='{.clusters[].cluster.server}')
+HTTPS="https://"
+IP=${IP[@]//${HTTPS}}
+IP=${IP[@]//:*}
+
+echo "Sending request to create prometheus snapshot"
+curl -XPOST http://${IP}:30000/api/v1/admin/tsdb/snapshot
+
+echo -e "\nDownloading snapshots from prometheus to "${DESTINATION_DIR}
+kubectl -n ${ONAP_NAMESPACE} cp  ${PROMETHEUS_POD_NAME}:/prometheus/snapshots ${DESTINATION_DIR}
+
